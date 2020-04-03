4431c95a69b87a9f45cf42e221a317b936a955bd
Developedbypatels <developedbypatels@gmail.com>
2020-04-03 16:57:34 -0400

    test

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -14,7 +14,7 @@ class CheckoutController extends Controller
     public function index()
     {
         $cart = session('test');
-        return view('index',['carts' => $cart]);
+        return view('checkout',['carts' => $cart]);
     }
 
     /**
