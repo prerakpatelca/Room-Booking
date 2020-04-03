9f6000012e0548423e2aaa574fd7e87697189556
Developedbypatels <developedbypatels@gmail.com>
2020-04-03 11:46:45 -0400

    test

--- app/Http/Controllers/Products.php
+++ app/Http/Controllers/Products.php
@@ -60,6 +60,8 @@ class Products extends Controller
      */
     public function edit(Product $product)
     {
+        $product = collect([1,2,3,4]);
+        Session::push('cart', $product);
         return $product;
     }
 
