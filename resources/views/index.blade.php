6ec7561ace574e5a926e799fc5e8ce01c4071350
Developedbypatels <developedbypatels@gmail.com>
2020-04-02 22:45:34 -0400

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -64,7 +64,7 @@
                             <div class="bottom-area d-flex px-3">
                                 <div class="m-auto d-flex">
                                     <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
-                                        <span>+/span>
+                                        <span>+</span>
                                     </a>
                                     <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
                                         <span><i class="ion-ios-cart"></i></span>
