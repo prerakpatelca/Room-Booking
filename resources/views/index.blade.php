54e3bd328a08826d899c43d6b09424acf5431cc6
Developedbypatels <developedbypatels@gmail.com>
2020-04-02 22:32:49 -0400

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -352,14 +352,14 @@
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
-  <script src="{{ asset('public/js/jquery.min.js') }}"></script>
-  <script src="{{ URL::asset('public/js/jquery-migrate-3.0.1.min.js') }}"></script>
-  <script src="{{ URL::asset('public/js/jquery.waypoints.min.js') }}"></script>
-  <script src="{{ URL::asset('public/js/jquery.stellar.min.js') }}"></script>
-  <script src="{{ URL::asset('public/js/owl.carousel.min.js') }}"></script>
-  <script src="{{ URL::asset('public/js/aos.js') }}"></script>
-  <script src="{{ URL::asset('public/js/scrollax.min.js') }}"></script>
-  <script src="{{ URL::asset('public/js/main.js') }}"></script>
-  <script src="{{ URL::asset('public/js/app.js') }}" defer></script>
+  <script src="/js/jquery.min.js"></script>
+  <script src="/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="/js/jquery.waypoints.min.js"></script>
+  <script src="/js/jquery.stellar.min.js"></script>
+  <script src="/js/owl.carousel.min.js"></script>
+  <script src="/js/aos.js"></script>
+  <script src="/js/scrollax.min.js"></script>
+  <script src="/js/main.js"></script>
+  <script src="/js/app.js" defer></script>
   </body>
 </html>
