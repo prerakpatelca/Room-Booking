b234f1c59121121c09c253e7e93a346bd514527e
Developedbypatels <developedbypatels@gmail.com>
2020-04-02 23:23:23 -0400

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::get('/test', function () {
+    $users = DB::table('products')->get();
+
+    return $users;
 });
