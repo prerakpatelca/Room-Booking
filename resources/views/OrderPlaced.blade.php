6867f6c000de162c8937ecabab13773b6428403e
Developedbypatels <developedbypatels@gmail.com>
2020-04-04 19:48:43 -0400

    updating

--- resources/views/OrderPlaced.blade.php
+++ resources/views/OrderPlaced.blade.php
@@ -1,7 +1,32 @@
 <!-- StAuth10065: I Prerak Patel, 000825410 certify that this material is my original work. 
 No other person's work has been used without due acknowledgement. I have not made my work available to anyone else. -->
-@extends('app')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mohawk Cart - Fresh Foods</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+
+    <link href="/css/app.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="/css/animate.css">
+    <link rel="stylesheet" href="/css/magnific-popup.css">
+    <link rel="stylesheet" href="/css/ionicons.min.css"> 
+    <link rel="stylesheet" href="/css/flaticon.css">
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body class="goto-here">
+
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('goback') }}">Mohawk Cart</a></li>
+        </div>
+    </nav>
 
     <section class="ftco-section ftco-cart">
         <div class="container">
@@ -94,4 +119,16 @@ No other person's work has been used without due acknowledgement. I have not mad
         </div>
     </section>
 
-@endsection
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/jquery-migrate-3.0.1.min.js"></script>
+        <script src="/js/jquery.waypoints.min.js"></script>
+        <script src="/js/jquery.stellar.min.js"></script>
+        <script src="/js/owl.carousel.min.js"></script>
+        <script src="/js/aos.js"></script>
+        <script src="/js/scrollax.min.js"></script>
+        <script src="/js/main.js"></script>
+        <script src="/js/app.js" defer></script>
+    </body>
+</html>
