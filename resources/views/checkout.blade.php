482fa1e3d318f41bcfc8563e0a2461e0cbc9afdc
Developedbypatels <developedbypatels@gmail.com>
2020-04-04 19:07:47 -0400

    updating

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -100,20 +100,5 @@
                     </div>
             </form>
         </div>
-    </section>
-
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
-
-  <script src="/js/jquery.min.js"></script>
-  <script src="/js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="/js/jquery.waypoints.min.js"></script>
-  <script src="/js/jquery.stellar.min.js"></script>
-  <script src="/js/owl.carousel.min.js"></script>
-  <script src="/js/aos.js"></script>
-  <script src="/js/scrollax.min.js"></script>
-  <script src="/js/main.js"></script>
-  <script src="/js/app.js" defer></script>
-  </body>
-</html>
-
+    </section
 @endsection
