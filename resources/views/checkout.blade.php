47ff9f0d2495ed93159ef3249dfe083abc6f134c
Developedbypatels <developedbypatels@gmail.com>
2020-04-04 19:05:18 -0400

    updating

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Mohawk Cart - Fresh Foods</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+@extends('app')
+@section('content')
 
-    <link href="/css/app.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/magnific-popup.css">
-    <link rel="stylesheet" href="/css/ionicons.min.css"> 
-    <link rel="stylesheet" href="/css/flaticon.css">
-    <link rel="stylesheet" href="/css/style.css">
-  </head>
-  <body class="goto-here">
-
-    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('products.index') }}">Mohawk Cart</a></li>
-        </div>
-    </nav>
-    
     <section class="ftco-section ftco-cart">
         <div class="container">
                 <div class="col-md-12 heading-section text-center ftco-animate">
@@ -140,3 +115,5 @@
   <script src="/js/app.js" defer></script>
   </body>
 </html>
+
+@endsection
