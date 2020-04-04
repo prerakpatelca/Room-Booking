257a8f8fdc9ef2d31c27f7909ff9229b3d8dbf1c
Developedbypatels <developedbypatels@gmail.com>
2020-04-04 18:34:49 -0400

    updating

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -109,7 +109,7 @@
             </div>
             </div>
             <br>
-            <p align="center"><a href="{{ route('checkout.index') }}" class="btn btn-primary py-3 px-4">{{count(session['test'])}} Proceed to Checkout</a></p>
+            <p align="center"><a href="{{ route('checkout.index') }}" class="btn btn-primary py-3 px-4">{{count(session('test'))}} Proceed to Checkout</a></p>
         </section>
   
 
