d0f6a60d3e5c7f265c4a8f01e7e58b10f6fc25bf
Developedbypatels <developedbypatels@gmail.com>
2020-04-04 18:50:51 -0400

    updating

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Mohawk Cart - VegeFoods</title>
+    <title>Mohawk Cart - Fresh Foods</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
